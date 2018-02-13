cb7b80c283b2c8137a6ee290f4cd1e40e3146b03
azder <azhder@gmail.com>
2018-02-13 06:55:43 +0100

    add `objects.js`

---
+++ src/objects.js
@@ -0,0 +1,52 @@
+const {_props_, _protos_, _metas_} = require('./symbols');
+const {X$nil} = require('./predicates');
+const {X$len} = require('./arrays');
+const {X$itr2set} = require('./transformers');
+
+const X$O = (
+    () => Object.create(null)
+);
+
+
+const X$toses = (
+    ($) => $[_protos_]
+);
+
+const X$props = (
+    ($) => $[_props_]
+);
+
+const X$metas = (
+    ($) => $[_metas_]
+);
+
+
+const X$length = (
+    ($) => X$len(X$props($))
+);
+
+
+const X$df = (
+    ($, v) => X$nil(v) ? $ : v
+);
+
+const X$has = (
+    ($, k) => (
+        /**@type{function(*=): Set<*>}*/
+        X$itr2set(X$props($))
+    ).has(k)
+);
+
+
+module.exports = ({
+    X$O,
+
+    X$toses,
+    X$props,
+    X$metas,
+
+    X$length,
+
+    X$df,
+    X$has,
+});
