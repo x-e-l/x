42ea1a4bd4330e98dfabe08fc117a39a796edc75
azder <azhder@gmail.com>
2018-02-13 05:45:50 +0100

    add `symbols.js`

---
+++ src/symbols.js
@@ -0,0 +1,26 @@
+module.exports = ([
+    // begin
+
+    // properties
+    '_metas_', '_props_',
+
+    //
+    '_protos_',
+    '_call_',
+    '_2str_',
+
+    '_length_',
+    '_mutas_',
+    '_atype_',
+
+    // constructor spec
+    '_key_', '_val_',
+
+    // archetypes
+    '_nil_', '_arr_', '_fun_', '_cst_', '_obj_',
+
+    // end
+].reduce(
+    ($, k) => ($[k] = Symbol(k)),
+    {}
+));
