53a61e397395fe14dc2baa3c21736cc9cb41c800
azder <azhder@gmail.com>
2018-02-16 07:06:57 +0100

    add `X$Nil`

--- src/constructors.js
+++ src/constructors.js
@@ -1,9 +1,9 @@
-const {_atype_, _obj_, _2str_} = require('./symbols');
+const {_atype_, _obj_, _nil_, _2str_} = require('./symbols');
 
 const {X$nil} = require('./predicates');
 const {X$reduce} = require('./arrays');
 const {X$padd, X$nset, X$mset} = require('./setters');
-const {X$obj2str} = require('./stringers');
+const {X$obj2str, X$nil2str} = require('./stringers');
 
 
 const X$O = (
@@ -24,7 +24,20 @@ const X$Obj = (function Obj($, ...$$) {
 
 });
 
+const X$Nil = (function Nil($, ...$$) {
+
+    $ = X$Obj($, ...$$);
+    $ = X$padd($, Nil);
+
+    X$mset($, _atype_, _nil_);
+    X$mset($, _2str_, X$nil2str);
+
+    return $;
+
+});
+
 
 module.exports = ({
     X$Obj,
+    X$Nil,
 });
--- xamples/tiny/cst.js
+++ xamples/tiny/cst.js
@@ -1,17 +1,30 @@
 const {X$call} = require('../../src/functions');
-const {X$mget, X$callf, X$2strf, X$toses, X$props} = require('../../src/getters');
+const {X$kv2ntry} = require('../../src/transformers');
+const {X$2strf, X$toses, X$props} = require('../../src/getters');
 
-const {X$Obj} = require('../../src/constructors');
+const {X$Obj, X$Nil} = require('../../src/constructors');
 
-const o = X$Obj();
-console.log(o);
-
+const o = X$Obj(
+    null,
+    X$kv2ntry('a', 1),
+    X$kv2ntry('b', 2)
+);
 
 console.log({
-    ['o.*toses']:  X$toses(o),
-    ['o.*props']:  X$props(o),
-    ['o.*2str']:   X$2strf(o),
-    ['o.*2str()']: X$call(X$2strf(o), o),
-    // ['o.*call']:   X$callf(o),
-    // ['o()']:       call(X$callf(o), o),
+    o,
+    'o.*toses':  X$toses(o),
+    'o.*props':  X$props(o),
+    'o.*2str':   X$2strf(o),
+    'o.*2str()': X$call(X$2strf(o), o),
+});
+
+
+const n = X$Nil(null, ...['a', 'b'].map(X$kv2ntry));
+
+console.log({
+    n,
+    'n.*toses':  X$toses(n),
+    'n.*props':  X$props(n),
+    'n.*2str':   X$2strf(n),
+    'n.*2str()': X$call(X$2strf(n), n),
 });
