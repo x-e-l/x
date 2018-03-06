155469ebb7d34d85f8ee5f3a84ae301c0c0d9ee0
azder <azhder@gmail.com>
2018-03-06 08:48:23 +0100

    add `nil.spec.js`

---
+++ tests/constructors/nil.spec.js
@@ -0,0 +1,104 @@
+const {
+    _metas_, _props_, _2str_, _toses_, _call_, _atype_,
+    _nil_, _cst_,
+    _key_, _val_,
+} = require('../../src/symbols');
+
+const {X$nil2str, X$cst2str} = require('../../src/stringers');
+
+const {X$Nil, Obj, Fun, Cst, Nil} = require('../../src/constructors');
+
+describe('constructors.Nil', () => {
+
+    it('is a constructor', () => {
+
+        expect(typeof X$Nil).toEqual('function');
+
+        const metas = X$Nil[_metas_];
+
+        expect(metas[_toses_]).toEqual([Obj, Fun, Cst]);
+        expect(metas[_atype_]).toBe(_cst_);
+        expect(metas[_2str_]).toBe(X$cst2str);
+
+        expect('' + metas[_call_]).toBe('($, ...$$) => X$padd(f($, ...$$), f)');
+
+    });
+
+    it('returns an object with no prototype', () => {
+
+        expect(X$Nil().prototype).toBe(void 0);
+
+    });
+
+    it('paramless returns an object with no keys, except default _metas_', () => {
+
+        const metas = {
+            [_atype_]: _nil_,
+            [_2str_]:  X$nil2str,
+            [_toses_]: [Obj, Nil],
+        };
+
+        const object = X$Nil();
+
+        expect(Object.keys(object)).toEqual([]);
+
+        expect(object[_metas_]).toEqual(metas);
+
+    });
+
+    it('with params returns an object with specified keys, existing keys and expected _metas_', () => {
+
+        const props = ['c', 'a', 'b'];
+
+        const metas = {
+            [_props_]: props,
+            [_atype_]: _nil_,
+            [_2str_]:  X$nil2str,
+            [_toses_]: [Obj, Nil],
+        };
+
+        const obj = {
+            c: {c: 3},
+        };
+
+        const object = X$Nil(
+            obj,
+            {[_key_]: 'a', [_val_]: 1},
+            {[_key_]: 'b', [_val_]: 2},
+        );
+
+        const keys = Object.keys(object);
+
+        expect(keys.length).toBe(props.length);
+        expect(object[_metas_][_props_]).toEqual(props);
+        expect(object[_metas_]).toEqual(metas);
+
+    });
+
+    it('returns an object with no keys, except default _metas_, for null', () => {
+
+        const metas = {
+            [_atype_]: _nil_,
+            [_2str_]:  X$nil2str,
+            [_toses_]: [Obj, Nil],
+        };
+
+        expect(X$Nil(null)).toEqual({[_metas_]: metas});
+
+    });
+
+    it('returns an object with no keys, except default _metas_, for undefined', () => {
+
+        const metas = {
+            [_atype_]: _nil_,
+            [_2str_]:  X$nil2str,
+            [_toses_]: [Obj, Nil],
+        };
+
+        expect(X$Nil()).toEqual({[_metas_]: metas});
+
+    });
+
+});
+
+
